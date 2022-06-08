efda3f42dced0bed85ef3e8512d5423cc753d180
dmnorlv <dmnorlv21@gmail.com>
2022-06-08 10:33:14 +0500

    исправил функции на стрелочные.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
-//диапазон может быть только положительный, включая ноль.
-//придумайте, как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
-//Функция может не гарантировать верный результат, если в переданном диапазоне нет ни одного подходящего числа.
-
 //источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomNumber(min, max) {
+const getRandomNumber = (min, max) => {
   if (Number.isFinite(min) && Number.isFinite(max) && (max > min) && (Math.sign(min) >= 0) && (Math.sign(max) >= 0)) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -24,10 +18,6 @@ function getRandomNumber(min, max) {
   return 'В переданном диапазоне нет ни одного подходящего числа!';
 }
 
-//Функция для проверки максимальной длины строки.
-//длина комментария не может составлять больше 140 символов;
-//должна быть универсальна.
-
-function checkStrokeLength(stroke, maxLength) {
+const checkStrokeLength = (stroke, maxLength) => {
   return (stroke.length <= maxLength);
 }
