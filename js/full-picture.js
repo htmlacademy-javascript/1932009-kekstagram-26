6e0220c1b671efe2b42cdba49b496dcd3994dd77
dmnorlv <dmnorlv21@gmail.com>
2022-07-08 13:06:05 +0500

    доработки.

--- js/full-picture.js
+++ js/full-picture.js
@@ -6,19 +6,16 @@ const imgWindow = document.querySelector('.big-picture');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 const buttonClose = imgWindow.querySelector('.big-picture__cancel');
 
-const addComments = (object) => {
-  for (let i=0; i<object.comments.length; i++){
-    const comment = commentTemplate.cloneNode(true);
-    comment.querySelector('.social__picture').src = object.comments[i].avatar;
-    comment.querySelector('.social__picture').alt = object.comments[i].name;
-    comment.querySelector('.social__text').textContent = object.comments[i].message;
-    imgWindow.querySelector('.social__comments').append(comment);
-  }
-};
+const addComments = (comments) => comments.forEach((element) => {
+  const comment = commentTemplate.cloneNode(true);
+  comment.querySelector('.social__picture').src = element.avatar;
+  comment.querySelector('.social__picture').alt = element.name;
+  comment.querySelector('.social__text').textContent = element.message;
+  imgWindow.querySelector('.social__comments').append(comment);
+});
 
-const openFullPicture = function(miniature, object) {
+const openFullPicture = (miniature, object, comments) => {
   miniature.addEventListener('click', () => {
-    imgWindow.classList.remove('hidden');
     imgWindow.querySelector('.big-picture__img').querySelector('img').src = object.url;
     imgWindow.querySelector('.likes-count').textContent = object.likes;
     imgWindow.querySelector('.comments-count').textContent = object.comments.length;
@@ -26,31 +23,30 @@ const openFullPicture = function(miniature, object) {
     imgWindow.querySelector('.social__comment-count').classList.add('hidden');
     imgWindow.querySelector('.comments-loader').classList.add('hidden');
     document.querySelector('body').classList.add('modal-open');
-    addComments(object);
+    addComments(comments);
+    imgWindow.classList.remove('hidden');
   });
 };
 
 for (let i=0; i<miniatures.length; i++) {
-  openFullPicture(miniatures[i], similarObjects[i]);
+  openFullPicture(miniatures[i], similarObjects[i], similarObjects[i].comments);
 }
 
-
-buttonClose.addEventListener('click', () => {
+const closeImgWindow = () => {
+  const commentsList = imgWindow.querySelector('.social__comments');
   document.querySelector('body').classList.remove('modal-open');
   imgWindow.classList.add('hidden');
-  while (imgWindow.querySelector('.social__comments').firstChild) {
-    imgWindow.querySelector('.social__comments')
-      .removeChild(imgWindow.querySelector('.social__comments').firstChild);
+  while (commentsList.firstChild) {
+    commentsList.removeChild(commentsList.firstChild);
   }
+};
+
+buttonClose.addEventListener('click', () => {
+  closeImgWindow();
 });
 
 document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
-    document.querySelector('body').classList.remove('modal-open');
-    imgWindow.classList.add('hidden');
-    while (imgWindow.querySelector('.social__comments').firstChild) {
-      imgWindow.querySelector('.social__comments')
-        .removeChild(imgWindow.querySelector('.social__comments').firstChild);
-    }
+    closeImgWindow();
   }
 });
