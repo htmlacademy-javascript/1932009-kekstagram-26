41f65f753371220ef22c7accb6a0039b035460e2
Keks <keks@htmlacademy.ru>
2022-07-12 11:23:39 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/full-picture.js
@@ -0,0 +1,57 @@
+// Отрисовка окна с полноразмерным изображением
+
+import {miniatures, similarObjects} from './miniatures.js';
+import {isEscapeKey} from './util.js';
+
+const imgWindow = document.querySelector('.big-picture');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const buttonClose = imgWindow.querySelector('.big-picture__cancel');
+
+const addComments = (comments) => comments.forEach((element) => {
+  const comment = commentTemplate.cloneNode(true);
+  comment.querySelector('.social__picture').src = element.avatar;
+  comment.querySelector('.social__picture').alt = element.name;
+  comment.querySelector('.social__text').textContent = element.message;
+  imgWindow.querySelector('.social__comments').append(comment);
+});
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeImgWindow();
+  }
+};
+
+const openImgWindow = (miniature, object, comments) => {
+  miniature.addEventListener('click', () => {
+    imgWindow.querySelector('.big-picture__img').querySelector('img').src = object.url;
+    imgWindow.querySelector('.likes-count').textContent = object.likes;
+    imgWindow.querySelector('.comments-count').textContent = object.comments.length;
+    imgWindow.querySelector('.social__caption').textContent = object.description;
+    imgWindow.querySelector('.social__comment-count').classList.add('hidden');
+    imgWindow.querySelector('.comments-loader').classList.add('hidden');
+    document.querySelector('body').classList.add('modal-open');
+    addComments(comments);
+    imgWindow.classList.remove('hidden');
+
+    document.addEventListener('keydown', onDocumentKeydown);
+  });
+};
+
+function closeImgWindow () {
+  const commentsList = imgWindow.querySelector('.social__comments');
+  document.querySelector('body').classList.remove('modal-open');
+  imgWindow.classList.add('hidden');
+  while (commentsList.firstChild) {
+    commentsList.removeChild(commentsList.firstChild);
+  }
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+for (let i=0; i<miniatures.length; i++) {
+  openImgWindow(miniatures[i], similarObjects[i], similarObjects[i].comments);
+}
+
+buttonClose.addEventListener('click', () => {
+  closeImgWindow();
+});
---
+++ 8/js/upload-form.js
@@ -0,0 +1,82 @@
+// Работа формы загрузки фото
+import {isEscapeKey} from './util.js';
+
+const uploadForm = document.querySelector('.img-upload__form');
+const uploadWindow = uploadForm.querySelector('.img-upload__overlay');
+const uploadInput = document.querySelector('#upload-file');
+const closeButton = uploadWindow.querySelector('#upload-cancel');
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt) && typeof evt.target.value !== 'string') {
+    closeUploadWindow();
+  }
+};
+
+const openUploadWindow = () => {
+  uploadInput.addEventListener('change', (evt) => {
+    const file = evt.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function () {
+      uploadWindow.querySelector('.img-upload__preview').querySelector('img').src = reader.result;
+    };
+    uploadWindow.classList.remove('hidden');
+    document.querySelector('body').classList.add('modal-open');
+
+    document.addEventListener('keydown', onDocumentKeydown);
+  });
+};
+openUploadWindow();
+
+function closeUploadWindow() {
+  uploadWindow.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  uploadInput.value = '';
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+closeButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  closeUploadWindow();
+});
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'field--invalid',
+  successClass: 'field--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'field__error'
+});
+
+const commentsField = uploadForm.querySelector('#description');
+const validateComments = (value) => value.length < 140;
+pristine.addValidator(commentsField, validateComments, 'Вы ввели более 140 символов!');
+
+const hashtagsField = uploadForm.querySelector('#hashtags');
+const validateHashtags = (value) => {
+  const strings = value.split(' ');
+  const re = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}/;
+  let hashtags = '';
+  for (let i=0; i<strings.length; i++) {
+    if (strings[i] === '') {
+      return true;
+    }
+    if (re.test(strings[i]) && strings.length <= 5 && !strings.some((string) => string === '')) {
+      if (!hashtags.includes(strings[i])) {
+        hashtags +=  strings[i];
+      } else {
+        return false;     // хэштэги не должны повторяться!
+      }
+    } else {
+      return false;     // введены некорректные данные/хэштегов более пяти
+    }
+  }
+  return true;      // ОК
+};
+pristine.addValidator(hashtagsField, validateHashtags, 'Введенные данные неверные!');
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
